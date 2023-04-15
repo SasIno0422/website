5d5a6d139bea6dac00b32c54b3637a8f9eaaa591
daddygi <vlimbaga49@gmail.com>
2023-04-15 22:40:54 +0800

    eman

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Login from "../components/Login";
+export default function LoginPage() {
+  return (
+    <main>
+      <div className="bg-gray-600">
+        <Login />
+      </div>
+    </main>
+  );
+}
